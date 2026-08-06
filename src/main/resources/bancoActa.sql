9a82b00f217efc875a0c85a58ad14c4fa8f83ebd
Cauê Alvarez <caue.20260048@institutojef.org.br>
2026-08-06 08:54:11 -0300

    Foi feito alterações nos ENUMs, mudança do nome da tabela projeto para ciclo, adição de UNIQUEs em Cpf e cnpj e NOT NULLs em atributos

--- src/main/resources/bancoActa.sql
+++ src/main/resources/bancoActa.sql
@@ -1,11 +1,11 @@
 set timezone to 'America/Sao_Paulo';
 
 -- ENUMs
-CREATE TYPE status_enum AS ENUM ('Não iniciado', 'Iniciado', 'Finalizado');
-CREATE TYPE status_meta_enum AS ENUM ('Abaixo do esperado', 'Regular', 'Acima do esperado');
-CREATE TYPE status_problema_enum AS ENUM ('Em análise', 'Em resolução', 'Resolvido');
-CREATE TYPE prioridade_enum AS ENUM ('Alto', 'Médio', 'Baixo');
-
+CREATE TYPE status_enum AS ENUM ('NAO_INICIADO', 'INICIADO', 'FINALIZADO');
+CREATE TYPE status_meta_enum AS ENUM ('ABAIXO_DO_ESPERADO', 'REGULAR', 'ACIMA_DO_ESPERADO');
+CREATE TYPE status_problema_enum AS ENUM ('EM_ANALISE', 'EM_RESOLUCAO', 'RESOLVIDO');
+CREATE TYPE prioridade_enum AS ENUM ('ALTO', 'MEDIO', 'BAIXO');
+CREATE TYPE etapas_ciclo_enum AS ENUM ('PLAN', 'DO', 'CHECK', 'ACT');
 -- Endereço
 CREATE TABLE IF NOT EXISTS endereco (
                                         endereco_id SERIAL PRIMARY KEY,
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS endereco (
 -- E-mail
 CREATE TABLE IF NOT EXISTS email (
                                      email_id SERIAL PRIMARY KEY,
-                                     email VARCHAR(80) NOT NULL
+                                     email VARCHAR(80) UNIQUE NOT NULL
 );
 
 -- Telefone
@@ -34,10 +34,10 @@ CREATE TABLE IF NOT EXISTS telefone (
 CREATE TABLE IF NOT EXISTS empresa (
                                        empresa_id SERIAL PRIMARY KEY,
                                        nome VARCHAR(30) NOT NULL,
-                                       setor VARCHAR(30),
-                                       unidade VARCHAR(30),
+                                       setor VARCHAR(30) NOT NULL,
+                                       unidade VARCHAR(30) NOT NULL,
                                        endereco_id INT REFERENCES endereco(endereco_id),
-                                       cnpj CHAR(14) NOT NULL
+                                       cnpj CHAR(14) UNIQUE NOT NULL
 );
 
 -- Administrador Geral
@@ -45,8 +45,7 @@ CREATE TABLE IF NOT EXISTS administrador_geral (
                                                    adm_geral_id SERIAL PRIMARY KEY,
                                                    nome VARCHAR(30) NOT NULL,
                                                    senha VARCHAR(100) NOT NULL,
-                                                   email_id INT NOT NULL REFERENCES email (email_id),
-                                                   cpf CHAR(11) NOT NULL
+                                                   email_id INT NOT NULL REFERENCES email (email_id)
 );
 
 -- Administração Empresa
@@ -56,7 +55,7 @@ CREATE TABLE IF NOT EXISTS administracao_empresa (
                                                      senha VARCHAR(100) NOT NULL,
                                                      email_id INT NOT NULL REFERENCES email(email_id),
                                                      empresa_id INT NOT NULL REFERENCES empresa(empresa_id),
-                                                     cpf CHAR(11) NOT NULL
+                                                     cpf CHAR(11) UNIQUE NOT NULL
 );
 
 -- Colaborador
@@ -68,17 +67,17 @@ CREATE TABLE IF NOT EXISTS colaborador (
                                            cargo VARCHAR(30) NOT NULL,
                                            email_id INT NOT NULL REFERENCES email(email_id),
                                            telefone_id INT NOT NULL REFERENCES telefone(telefone_id),
-                                           cpf CHAR(11) NOT NULL
+                                           cpf CHAR(11) UNIQUE NOT NULL
 );
 
 -- Projeto
-CREATE TABLE IF NOT EXISTS projeto (
-                                       projeto_id SERIAL PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS ciclo (
+                                       ciclo_id SERIAL PRIMARY KEY,
                                        nome VARCHAR(30) NOT NULL,
-                                       etapa_atual VARCHAR(10) NOT NULL,
+                                       etapa_atual etapas_ciclo_enum NOT NULL DEFAULT 'PLAN',
                                        dt_inicio DATE NOT NULL,
                                        dt_fim DATE,
-                                       status status_enum NOT NULL DEFAULT 'Não iniciado',
+                                       status status_enum NOT NULL DEFAULT 'NAO_INICIADO',
                                        criado_em TIMESTAMPTZ NOT NULL DEFAULT NOW()
 );
 
@@ -89,9 +88,9 @@ CREATE TABLE IF NOT EXISTS meta (
                                     descricao_meta TEXT,
                                     objetivo VARCHAR(200),
                                     prazo DATE NOT NULL,
-                                    status status_meta_enum NOT NULL DEFAULT 'Regular',
+                                    status status_meta_enum NOT NULL DEFAULT 'REGULAR',
                                     criado_em TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-                                    projeto_id INT NOT NULL REFERENCES projeto(projeto_id)
+                                    ciclo_id INT NOT NULL REFERENCES ciclo(ciclo_id)
 );
 
 -- Plano de Ação
@@ -99,9 +98,9 @@ CREATE TABLE IF NOT EXISTS plano_acao (
                                           plano_acao_id SERIAL PRIMARY KEY,
                                           nome VARCHAR(20) NOT NULL,
                                           descricao TEXT,
-                                          status status_enum NOT NULL DEFAULT 'Não iniciado',
-                                          prioridade prioridade_enum NOT NULL DEFAULT 'Médio',
-                                          projeto_id INT NOT NULL REFERENCES projeto(projeto_id)
+                                          status status_enum NOT NULL DEFAULT 'NAO_INICIADO',
+                                          prioridade prioridade_enum NOT NULL DEFAULT 'MEDIO',
+                                          ciclo_id INT NOT NULL REFERENCES ciclo(ciclo_id)
 );
 
 -- 5W2H
@@ -122,10 +121,10 @@ CREATE TABLE IF NOT EXISTS tarefa (
                                       tarefa_id SERIAL PRIMARY KEY,
                                       titulo VARCHAR(20) NOT NULL,
                                       descricao TEXT,
-                                      prioridade prioridade_enum NOT NULL DEFAULT 'Médio',
+                                      prioridade prioridade_enum NOT NULL DEFAULT 'MEDIO',
                                       dt_entrega DATE,
-                                      status status_enum NOT NULL DEFAULT 'Não iniciada',
-                                      dt_inicio DATE NOT NULL,
+                                      status status_enum NOT NULL DEFAULT 'NAO_INICIADO',
+                                      dt_inicio DATE NOT NULL DEFAULT now(),
                                       colaborador_id INT NOT NULL REFERENCES colaborador(colaborador_id)
 );
 
@@ -139,7 +138,7 @@ CREATE TABLE IF NOT EXISTS licoes_aprendidas (
                                                  descricao TEXT,
                                                  fase_origem VARCHAR(8),
                                                  severidade VARCHAR(50),
-                                                 colaborador_id INT NOT NULL REFERENCES colaborador(colaborador_id)
+                                                 ciclo_id INT NOT NULL REFERENCES ciclo(ciclo_id)
 );
 
 -- Problema
@@ -151,22 +150,22 @@ CREATE TABLE IF NOT EXISTS problema (
                                         status status_problema_enum NOT NULL DEFAULT 'Em análise',
                                         origem TEXT,
                                         encontrado_em DATE NOT NULL,
-                                        projeto_id INT NOT NULL REFERENCES projeto(projeto_id),
+                                        ciclo_id INT NOT NULL REFERENCES ciclo(ciclo_id),
                                         plano_acao_id INT REFERENCES plano_acao(plano_acao_id)
 );
 
 -- Relacionamentos N:N
 
-CREATE TABLE IF NOT EXISTS projeto_colaborador (
-                                                   projeto_id INT NOT NULL REFERENCES projeto (projeto_id),
+CREATE TABLE IF NOT EXISTS ciclo_colaborador (
+                                                   ciclo_id INT NOT NULL REFERENCES ciclo (ciclo_id),
                                                    colaborador_id INT NOT NULL REFERENCES colaborador (colaborador_id),
-                                                   PRIMARY KEY (projeto_id, colaborador_id)
+                                                   PRIMARY KEY (ciclo_id, colaborador_id)
 );
 
-CREATE TABLE IF NOT EXISTS projeto_plano_acao (
-                                                  projeto_id INT NOT NULL REFERENCES projeto (projeto_id),
+CREATE TABLE IF NOT EXISTS ciclo_plano_acao (
+                                                  ciclo_id INT NOT NULL REFERENCES ciclo (ciclo_id),
                                                   plano_acao_id INT NOT NULL REFERENCES plano_acao (plano_acao_id),
-                                                  PRIMARY KEY (projeto_id, plano_acao_id)
+                                                  PRIMARY KEY (ciclo_id, plano_acao_id)
 );
 
 CREATE TABLE IF NOT EXISTS tarefa_plano_acao (
@@ -190,19 +189,16 @@ ADD CONSTRAINT chk_tamanho_telefone CHECK (length(telefone)=11);
 ALTER TABLE empresa
 ADD CONSTRAINT chk_cnpj_empresa CHECK (length(cnpj)=14);
 
-ALTER TABLE administrador_geral
-ADD CONSTRAINT chk_cpf_adm_geral CHECK ( length(cpf)=11);
-
 ALTER TABLE administracao_empresa
 ADD CONSTRAINT chk_cpf_adm_empresa CHECK ( length(cpf)=11);
 
 ALTER TABLE colaborador
 ADD CONSTRAINT chk_cpf_colab CHECK ( length(cpf)=11);
 
-ALTER TABLE projeto
+ALTER TABLE ciclo
 ADD CONSTRAINT chk_dt_inicio CHECK ( dt_inicio>=current_date);
 
-ALTER TABLE projeto
+ALTER TABLE ciclo
 ADD CONSTRAINT chk_dt_fim CHECK (dt_fim>=dt_inicio);
 
 ALTER TABLE meta
